81a473fde990a2ef171cecad1d63cfb07d1ea3a2
jemmyblair <42910234+jamesmhee@users.noreply.github.com>
2024-07-12 19:21:43 +0700

    edit zindex

--- src/components/PleaseConnect.tsx
+++ src/components/PleaseConnect.tsx
@@ -2,7 +2,7 @@ import ConnectWalletButton from "../components/ConnectWalletButton"
 
 const PleaseConnect = () => {
   return (
-    <div className="w-screen h-screen bg-black z-10 fixed top-0 flex items-center justify-center">        
+    <div className="w-screen h-screen bg-black z-[60] fixed top-0 flex items-center justify-center">        
         <div className="bg-zinc-900 p-2 rounded-xl w-[calc(100vw_-_50px)] sm:max-w-sm md:max-w-lg h-80 text-white">            
             <div className="w-full h-full  backdrop-blur  border-2 border-zinc-800 rounded-lg flex items-center justify-center flex-col">
                 <h2 className="text-xl my-5 uppercase text-zinc-300">Please connect wallet</h2>
